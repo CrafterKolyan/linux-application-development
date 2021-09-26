34d67ea50b29e0bad916bf4ef2a5e11b3604921e
Nikolay Korolev <korolevns98@gmail.com>
2021-09-27 00:26:54 +0300

    Some code

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include <locale.h>
 
 #ifdef _WIN32
@@ -8,6 +8,8 @@
 #endif
 
 #define KEY_ESCAPE 27
+
+const size_t BUFFER_SIZE = 4096;
 
 void print_help(char *program_name) {
     fprintf(stderr, "Usage: %s <filename>\n", program_name);
@@ -19,7 +21,23 @@ WINDOW *create_window(char *window_name) {
     scrollok(window, TRUE);
     box(window, 0, 0);
     mvwprintw(window, 0, 2, "%s", window_name);
+    wmove(window, 1, 1);
+    waddnstr(window, "Hello therajskdhsakjdhkjashdkjashdjka", COLS - 2);
     return window;
+}
+
+char **read_file(char *filename) {
+    FILE *file = fopen(filename, "r");
+    char buffer[BUFFER_SIZE];
+    size_t sz = 0;
+
+    size_t total_lines = 1;
+    char **lines = calloc(1, sizeof(*lines));
+    while(sz = fread(buffer, sizeof(buffer) / sizeof(*buffer), 1, file)) {
+
+    }
+    fclose(file);
+    return lines;
 }
 
 int main(int argc, char *argv[]) {
@@ -35,10 +53,18 @@ int main(int argc, char *argv[]) {
     cbreak();
     
     WINDOW *window = create_window(filename);
-    int c;
-    while((c = wgetch(window)) != KEY_ESCAPE) {
-        box(window, 0, 0); 
-        wrefresh(window);
-    }
+    bool done = false;
+    do {
+        werase(window);
+        switch (wgetch(window))
+        {
+        case KEY_ESCAPE:
+            done = true;
+            break;
+        
+        default:
+            break;
+        }
+    } while (!done);
     return endwin();
 }
