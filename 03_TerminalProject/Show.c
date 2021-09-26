cae08c837cd36ab04645c20b4eab1893ba7b1149
Nikolay Korolev <korolevns98@gmail.com>
2021-09-27 00:12:38 +0300

    Better code

--- 03_TerminalProject/Show.c
+++ 03_TerminalProject/Show.c
@@ -1,5 +1,44 @@
 #include <stdio.h>
+#include <locale.h>
 
-int main() {
-    printf("Hello World!");
+#ifdef _WIN32
+#   include <curses.h>
+#else
+#   include <ncurses.h>
+#endif
+
+#define KEY_ESCAPE 27
+
+void print_help(char *program_name) {
+    fprintf(stderr, "Usage: %s <filename>\n", program_name);
+}
+
+WINDOW *create_window(char *window_name) {
+    WINDOW *window = newwin(0, 0, 0, 0);
+    keypad(window, TRUE);
+    scrollok(window, TRUE);
+    box(window, 0, 0);
+    mvwprintw(window, 0, 2, "%s", window_name);
+    return window;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        print_help(argv[0]);
+        return 0;
+    }
+    char *filename = argv[1];
+
+    setlocale(LC_ALL, "");
+    initscr();
+    noecho();
+    cbreak();
+    
+    WINDOW *window = create_window(filename);
+    int c;
+    while((c = wgetch(window)) != KEY_ESCAPE) {
+        box(window, 0, 0); 
+        wrefresh(window);
+    }
+    return endwin();
 }
