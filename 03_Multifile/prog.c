b5d38e11c35ee92fcc519bc23a83f778d1a758e7
Nikolay Korolev <korolevns98@gmail.com>
2021-10-07 11:52:15 +0300

    Update files with newer version

--- 03_Multifile/prog.c
+++ 03_Multifile/prog.c
@@ -1,9 +1,17 @@
+#include <stdio.h>
 #include "outlib.h"
 
 int main(int argc, char *argv[]) {
     Count = argc;
-    output("<INIT>");
-    output(argc>1 ? argv[1] : "<NOPE>");
-    output("<DONE>");
+    if (Count > 1) {
+        output("<INIT>");
+        for(int i = 1; i < argc; i++) {
+            output(argv[i]);
+        }
+        output("<DONE>");
+    }
+    else {
+        usage(argv[0]);
+    }
     return 0;
 }
